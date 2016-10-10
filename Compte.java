8c50e183b4c91ce605e4b322d9378cb8ce19b9a8
MF1996 <makam-elchahid@outlook.fr>
2016-10-10 17:50:27 +0100

    j'aii ajouter deux fichier java

---
+++ Compte.java
@@ -0,0 +1,61 @@
+package Compte_bancaire;
+
+import java.util.Scanner;
+
+public class Compte {
+	private double solde;
+	private String nom;
+	private String numcp;
+	private double intert;
+
+	public Compte(String numcp, String nom) {
+		this.numcp = numcp;
+		this.nom = nom;
+
+	}
+
+	public void afficher(int count) {
+		System.out.println("le compte " + (count + 1)
+				+ " son taux intert est de " + intert + "%");
+
+	}
+
+	public void intert(double intert) {
+
+		solde = solde + (solde * intert / 100) / 12;
+
+	}
+
+	public double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public double getIntert() {
+		return intert;
+	}
+
+	public void setIntert(double intert) {
+		this.intert = intert;
+	}
+
+	public String getNumcp() {
+		return numcp;
+	}
+
+	public void setNumcp(String numcp) {
+		this.numcp = numcp;
+	}
+
+}
